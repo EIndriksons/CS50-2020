8267cfac5b7457035ad2a9a131bab08c6bf414e7
EdgarEndre <edgars@indriksons.lv>
2020-04-24 21:13:09 +0300

    Pong 0 - day 0 update with comments

--- games/pong/main.lua
+++ games/pong/main.lua
@@ -1,6 +1,9 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+--[[
+    Runs when the game first starts up, only once; used to initialize the game.
+]]
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -9,6 +12,14 @@ function love.load()
     })
 end
 
+--[[
+    Called after update by LOVE, used to draw anything to the screen, updated or otherwise.
+]]
 function love.draw()
-    love.graphics.printf("Hello Pong!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
+    love.graphics.printf(
+        "Hello Pong!",              -- Text to render
+        0,                          -- starting X (0 since we're going to center it based on width)
+        WINDOW_HEIGHT / 2 - 6,      -- starting Y (halfway down the screen)
+        WINDOW_WIDTH,               -- number of pixels to center within (the entire screen here)
+        'center')                   -- alignment mode, can be 'center', 'left', or 'right'
 end
